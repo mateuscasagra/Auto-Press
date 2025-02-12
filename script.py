79794b7f97fc48744e32646b626085befe7b090c
mateuscasagra <mateus.zandonacasagrande@gmail.com>
2025-02-12 14:57:32 -0300

    struct

--- script.py
+++ script.py
@@ -11,35 +11,20 @@ def clickf2():
 
 contador = 0
 
-class status:
-    Interrompido = False
-    Encerrado = False
-    Iniciado = True
 
-
-#Inicia o script caso aperte 2 script pausa caso aperte 1 retoma caso aperta 3 encerra
 print("Sistema Inicializado...")
 
-while status.Iniciado == True:
+while True:
     clickf2()
+    contador+=1
+    print(f"F2 Apertado: {contador} vezes")
+    intervalo = random.randint(60,180)
+    print(f"Aguardando {intervalo} segundos...")
+    time.sleep(intervalo)
 
 
-opcao = input("(1)Retomaz\n(2)Interrompe\n(3)Encerra")
-def switch(opcao):
-    if opcao == 1 and status.Iniciado == True:
-        return"Sistema Rodando..."
-    
-    elif opcao == 1 and status.Interrompido == True:
-        status.Interrompido == False
-        status.Iniciado == True
 
-    elif opcao == 2 and status.Iniciado == True:
-        status.Iniciado == False
-        status.Interrompido == True
-        print("Sistema Interrompido...")
-        
-    else: print("Opcao Invalida")
-        
+
 
 
 
