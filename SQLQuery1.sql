db1bf3a9c3f45dc117733e53974c98b72264bb92
mateuscasagra <mateus.zandonacasagrande@gmail.com>
2025-02-12 17:30:35 -0300

    add procedure and table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,20 +1,49 @@
 CREATE DATABASE Auto_Press
 use Auto_Press;
 
-CREATE TABLE timeRecord(
-HorarioEntrada DATE NOT NULL,
-HorarioSaida DATE NOT NULL
-CONSTRAINT data_atualpk PRIMARY KEY(HorarioEntrada)
 
+INSERT timeRecord(HorarioEntrada, HorarioSaida)
+VALUES('2025-01-01 08:00:00','2025-01-01 08:00:00');
+
+INSERT workTime(Dia,totalHora);
+VALUES('2025-01-01 08:00:00','2025-01-01 08:00:00');
+
+CREATE TABLE timeRecord(
+	Id INT IDENTITY,
+	HorarioEntrada DATE NOT NULL,
+	HorarioSaida DATE
+	CONSTRAINT data_atualpk PRIMARY KEY(Id)
 );
 
-CREATE PROCEDURE insere_horario
+CREATE TABLE workTime(
+	Id INT IDENTITY,
+	Dia DATE NOT NULL,
+	totalHora HOUR NOT NULL
+	CONSTRAINT work_timepk PRIMARY KEY(Id)
+)
+
+CREATE PROCEDURE time_record_entrada
 AS 
 	INSERT INTO timeRecord
-	(HorarioEntrada, HorarioSaida)
-	
+	(HorarioEntrada)
 	SELECT DATEADD(HOUR, -3, GETDATE())
 
+END;
+GO
 
 
-GO;
+CREATE PROCEDURE time_record_saida
+AS 
+	INSERT INTO timeRecord
+	(HorarioSaida)
+	SELECT DATEADD(HOUR, -3, GETDATE())
+
+END;
+GO
+
+CREATE PROCEDURE work_time 
+AS 
+	INSERT INTO(Dia, totalHora)
+	SELECT GETDATE() as Dia, CASE ISNULL(totalHora) = GETDATE DATEADD(HOUR, -3, GETDATE())
+
+
--- script.py
+++ script.py
@@ -2,24 +2,36 @@ import time
 import random
 import pyautogui
 pyautogui.FAILSAFE = False
+import pyodbc
+
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                      'SERVER=localhost;'
+                      'DATABASE=seu_banco_de_dados;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
 
 
 def clickf2():
     pyautogui.press('f2')
 
 
-
 contador = 0
 
+
+tempo_inicial = time.time()
 
 print("Sistema Inicializado...")
 
 while True:
     clickf2()
-    contador+=1
+    contador += 1
     print(f"F2 Apertado: {contador} vezes")
-    intervalo = random.randint(60,180)
+    intervalo = random.randint(60, 120)
     print(f"Aguardando {intervalo} segundos...")
+    tempo_decorrido = time.time() - tempo_inicial
+    print(tempo_decorrido)
     time.sleep(intervalo)
 
 
