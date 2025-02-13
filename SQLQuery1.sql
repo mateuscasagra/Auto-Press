64a377777e403a44103c9f4ac5d2f347ad4244ae
mateuscasagra <mateus.zandonacasagrande@gmail.com>
2025-02-13 00:37:42 -0300

    :(

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,25 +5,26 @@ use Auto_Press;
 INSERT timeRecord(HorarioEntrada, HorarioSaida)
 VALUES('2025-01-01 08:00:00','2025-01-01 08:00:00');
 
-INSERT workTime(Dia,totalHora);
-VALUES('2025-01-01 08:00:00','2025-01-01 08:00:00');
+INSERT workTime(Dia,totalHora)
+VALUES('2025-01-01 08:00:00','08:00:00');
 
 CREATE TABLE timeRecord(
 	Id INT IDENTITY,
-	HorarioEntrada DATE NOT NULL,
-	HorarioSaida DATE
+	HorarioEntrada DATETIME NOT NULL,
+	HorarioSaida DATETIME
 	CONSTRAINT data_atualpk PRIMARY KEY(Id)
 );
 
 CREATE TABLE workTime(
 	Id INT IDENTITY,
-	Dia DATE NOT NULL,
-	totalHora HOUR NOT NULL
+	Dia DATETIME NOT NULL,
+	totalHora TIME NOT NULL
 	CONSTRAINT work_timepk PRIMARY KEY(Id)
-)
+);
 
-CREATE PROCEDURE time_record_entrada
-AS 
+CREATE PROCEDURE time_record
+AS
+BEGIN
 	INSERT INTO timeRecord
 	(HorarioEntrada)
 	SELECT DATEADD(HOUR, -3, GETDATE())
@@ -32,18 +33,14 @@ END;
 GO
 
 
-CREATE PROCEDURE time_record_saida
-AS 
-	INSERT INTO timeRecord
-	(HorarioSaida)
-	SELECT DATEADD(HOUR, -3, GETDATE())
 
+CREATE PROCEDURE work_time 
+AS
+BEGIN
+	INSERT INTO(Dia, totalHora)
+	SELECT GETDATE() as Dia, CASE ISNULL(totalHora) = GETDATE DATEADD(HOUR, -3, GETDATE())
 END;
 GO
 
-CREATE PROCEDURE work_time 
-AS 
-	INSERT INTO(Dia, totalHora)
-	SELECT GETDATE() as Dia, CASE ISNULL(totalHora) = GETDATE DATEADD(HOUR, -3, GETDATE())
 
 
--- script.py
+++ script.py
@@ -1,41 +1,44 @@
 import time
 import random
 import pyautogui
-pyautogui.FAILSAFE = False
 import pyodbc
+import keyboard  # Biblioteca para detectar a pressão de teclas
 
+pyautogui.FAILSAFE = False
 
+# Conexão com o banco de dados
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                      'SERVER=localhost;'
-                      'DATABASE=seu_banco_de_dados;'
+                      'SERVER=localhost\MSSQLSERVER01;'
+                      'DATABASE=Auto_Press;'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
 
-
 def clickf2():
     pyautogui.press('f2')
 
-
 contador = 0
-
-
-tempo_inicial = time.time()
+controle = True
 
 print("Sistema Inicializado...")
+cursor.execute("EXEC time_record")
 
-while True:
-    clickf2()
+while controle:
+    clickf2()  
     contador += 1
     print(f"F2 Apertado: {contador} vezes")
+    
+    
     intervalo = random.randint(60, 120)
     print(f"Aguardando {intervalo} segundos...")
-    tempo_decorrido = time.time() - tempo_inicial
-    print(tempo_decorrido)
     time.sleep(intervalo)
+    
+ 
+    if keyboard.is_pressed('esc'):
+        print("Tecla ESC pressionada. Finalizando...")
+        controle = False
 
-
-
+conn.close()  
 
 
 
