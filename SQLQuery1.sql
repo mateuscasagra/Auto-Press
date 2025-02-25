fd98f6300e3297c2ab83ea39778b83ba1bddf259
mateuscasagra <mateus.zandonacasagrande@gmail.com>
2025-02-25 13:46:45 -0300

    bd corrigido

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,21 +1,19 @@
-CREATE DATABASE Auto_Press
-use Auto_Press;
-
+CREATE DATABASE Auto_Press;
+USE Auto_Press;
 
 CREATE TABLE TimeRecord(
-	Dia DATE PRIMARY KEY,
-	TempoEntrada DATETIME,
-	TempoSaida DATETIME,
-	TotalHora DECIMAL(4,2)
+    Dia DATE PRIMARY KEY,
+    HoraEntrada TIME(0),
+    HoraSaida TIME(0),
+    TotalHora TIME(0)
 );
 
 CREATE TABLE Tasks(
-	Id INT PRIMARY KEY IDENTITY NOT NULL,
-	Nome VARCHAR(100) NOT NULL,
-	Descricao VARCHAR(500),
+    Id INT PRIMARY KEY IDENTITY NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Descricao VARCHAR(500),
 	[Status] BIT
 );
-
 
 CREATE TABLE TasksDay (
     TaskId INT,
@@ -25,3 +23,20 @@ CREATE TABLE TasksDay (
     FOREIGN KEY (Dia) REFERENCES TimeRecord(Dia)
 );
 
+GO
+
+CREATE PROCEDURE AddTime
+AS
+BEGIN
+    INSERT INTO TimeRecord(Dia, HoraEntrada)
+    SELECT GETDATE() as Dia, FORMAT(GETDATE(), 'HH:mm:ss') as HoraEntrada
+END;
+
+
+EXEC AddTime
+
+SELECT * FROM TimeRecord
+SELECT * FROM TasksDay
+delete from TimeRecord
+delete from TasksDay
+drop table TimeRecord
--- script.py
+++ script.py
@@ -1,41 +1,30 @@
 import time
 import random
 import pyautogui
-import pyodbc
-import keyboard  
 
 pyautogui.FAILSAFE = False
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                        'SERVER=localhost\MSSQLSERVER01;'
-                        'DATABASE=Auto_Press;'
-                        'Trusted_Connection=yes;')
-
-cursor = conn.cursor()
 
 def clickf2():
     pyautogui.press('f2')
 
 contador = 0
-controle = True
 
 print("Sistema Inicializado...")
-cursor.execute("EXEC time_record HorarioEntrada")
 
-while controle:
+
+while True:
     clickf2()  
     contador += 1
     print(f"F2 Apertado: {contador} vezes")
     intervalo = random.randint(60, 120)
     print(f"Aguardando {intervalo} segundos...")
     time.sleep(intervalo)
-    if keyboard.read_event(suppress=True).name == 'esc':
-        print("Interrompendo o loop...")
-        controle = False
     
 
-print("Loop Interrompido")
-cursor.execute("EXEC time_record HorarioSaida")
+
+
+
 
 
 
